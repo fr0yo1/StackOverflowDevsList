6b2f6009f77dac27583bbf971e850ea411c9f054
Adrian Sandru <adrian.sandru36@yahoo.com>
2019-06-10 01:08:20 +0300

    Added application flow.

--- app/src/main/java/com/stackoverflowdevslist/MainActivity.kt
+++ app/src/main/java/com/stackoverflowdevslist/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.stackoverflowdevslist
 
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation.findNavController
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+        DevList.OnFragmentInteractionListener,
+        DeveloperFragment.OnFragmentInteractionListener {
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
---
+++ app/src/main/java/com/stackoverflowdevslist/SplashFragment.kt
@@ -0,0 +1,40 @@
+package com.stackoverflowdevslist
+
+import android.os.Bundle
+import android.os.Handler
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation.findNavController
+
+class SplashFragment : Fragment() {
+    private var handler: Handler? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash_sceen, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        handler = Handler().apply {
+                    postDelayed({
+                    didFinishSplashScreen()
+                    },1500)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        handler!!.removeCallbacksAndMessages(null)
+    }
+
+    private fun didFinishSplashScreen() {
+        val navOptions = NavOptions.Builder().setPopUpTo(R.id.splash_screen,true).build()
+        findNavController(view!!).navigate(R.id.action_splash_screen_to_devList,null, navOptions)
+    }
+}
